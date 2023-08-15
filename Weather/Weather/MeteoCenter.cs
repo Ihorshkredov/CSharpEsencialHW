998d3b872da23ac5b61f62a1af36d066058de701
Igor Shkredov <Igor.shkredov@flex.com>
2023-08-16 00:53:42 +0300

    Added Homw work with structure and DateTime. There a lot of error handling gaps, but I play a lot with DataTime

---
+++ Weather/Weather/MeteoCenter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    internal class MeteoCenter
+    {
+        public Temperature [] MeteoData { get; }
+
+
+        public MeteoCenter(Weather weather)
+        {
+            MeteoData = MonitorWeather(weather);
+        }
+
+
+        public Temperature[] MonitorWeather(Weather weather)
+        {
+            Temperature[] temperatures = new Temperature [weather.Period];  
+
+            for (int i = 0; i < weather.Period ; i++)
+            {
+                temperatures[i] = new Temperature(weather.Temps[i],DateTime.Today.Date.AddDays(i+1));
+            }
+            return temperatures;
+        }
+
+        public void GetTemperatureByDate(DateTime date, out Temperature result)
+        {
+            result = new Temperature();
+
+            for (int i = 0; i < MeteoData.Length-1; i++)
+            {
+                if (MeteoData[i].Date == date)
+                {
+                   result= MeteoData[i];
+                }
+                else
+                {
+                    continue;
+                      
+                }
+            } 
+        }
+
+
+        public void GetTemperatureByPeriod(DateTime startDate, DateTime dueDate, out Temperature[] result)
+        {
+            result= new Temperature[dueDate.DayOfYear - startDate.DayOfYear];
+            int matchCounter = 0;
+            for (int i = 0; i < MeteoData.Length-1; i++)
+            {
+                if ((MeteoData[i].Date == startDate.Date.AddDays(matchCounter)) && (MeteoData[i].Date < dueDate.Date))
+                {
+                    result[matchCounter] = MeteoData[i];
+                    matchCounter++;
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            
+            
+        }
+
+    }
+}
---
+++ Weather/Weather/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Weather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Weather summerWeather = new Weather(10, "Summer"); //Generate summer weather for next 10 days
+            
+            MeteoCenter meteoCenter = new MeteoCenter(summerWeather);// Meteocenter analize generated weather and receive meteodata
+           
+            var requestDate = DateTime.Now.Date.AddDays(5);// request weather in 5 days after today
+
+            meteoCenter.GetTemperatureByDate(requestDate, out Temperature result);
+
+
+            var dateFrom = DateTime.Now.Date.AddDays(3);
+            var dateTo = DateTime.Now.Date.AddDays(7);
+
+
+            meteoCenter.GetTemperatureByPeriod(dateFrom,dateTo,out Temperature [] prognoze);
+
+            Console.ReadLine();
+            
+
+            
+        }
+    }
+}
