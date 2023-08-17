d3c58fcf5f2976c0361492fbd59d740d138a1f0e
Igor Shkredov <Igor.shkredov@flex.com>
2023-08-17 14:55:24 +0300

    Added separate class for Math actions. added bool flag in case we have incorrect action in order not to invoke delegate without ref

---
+++ Delegate_Calc/Delegate_Calc/MathActions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Calc
+{
+    public static class MathActions
+    {
+        public static double Add(double a, double b) => a + b;
+        public static double Sub(double a, double b) => a - b;
+        public static double Mult(double a, double b) => a * b;
+        public static double Div(double a, double b) => b == 0 ? 0 : a / b;
+
+    }
+}
--- Delegate_Calc/Delegate_Calc/Program.cs
+++ Delegate_Calc/Delegate_Calc/Program.cs
@@ -14,43 +14,44 @@
             Console.Write("Enter second value \'B\':");
             bool correctB = double.TryParse(Console.ReadLine(), out double b);
 
-            if (correctA&&correctB)
+            if (correctA && correctB)
             {
-                Console.WriteLine("\nPlease enter action for two values:\n \'+\' - Add,\n \'-\' - Substrakt, \n \'*\' - Multiply, \n \'/\' - Divide");
+                Console.WriteLine("\n Please enter action for two values:\n \'+\' - Add," +
+                    "\n \'-\' - Substrakt, \n \'*\' - Multiply, \n \'/\' - Divide");
                 Console.Write("Make your choise:");
                 string action = Console.ReadLine();
 
+                bool correctAction = true;
+                calculateDelegate = null;
                 switch (action)
                 {
                     case "+":
-                        calculateDelegate = new Calculate(Add);
+                        calculateDelegate = new Calculate(MathActions.Add);
                         break;
                     case "-":
-                        calculateDelegate = new Calculate(Sub);
+                        calculateDelegate = new Calculate(MathActions.Sub);
                         break;
                     case "*":
-                        calculateDelegate = new Calculate(Mult);
+                        calculateDelegate = new Calculate(MathActions.Mult);
                         break;
-                        case "/":
-                        calculateDelegate = new Calculate(Div);
+                    case "/":
+                        calculateDelegate = new Calculate(MathActions.Div);
                         break;
 
                     default:
-                        Console.WriteLine("Unknown action");
-                        calculateDelegate = null;
+                        correctAction = false;
+                        Console.WriteLine("Incorrect action");
                         break;
                 }
-                
-                Console.WriteLine("Result : " + calculateDelegate.Invoke(a,b).ToString());
+
+                if (correctAction)
+                {
+                    double result =  calculateDelegate.Invoke(a, b);
+                    Console.WriteLine($"Result  is ->  {result}");
+                } 
             }
-        }
-
-
-        private static double Add(double a, double b) => a + b;
-        private static double Sub(double a, double b) => a - b;
-        private static double Mult(double a, double b) => a * b;
-        private static double Div(double a, double b) => b == 0 ? 0 : a / b;
-
-        
+            else 
+            Console.WriteLine("Incorrect input");   
+        }    
     }
 }
